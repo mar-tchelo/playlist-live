939b9814db7b2817b9629fdf6e8e2b6f5afa2f3c
mar-tchelo <marcelo.js10@gmail.com>
2022-05-07 21:16:28 -0300

    getAll

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const connectToDb = require("./database/db");
 const path = require("path");
-const Music = require("./model/Music")
+const Music = require("./model/Music");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,21 +13,21 @@ app.use(express.urlencoded());
 
 connectToDb();
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const playlist = await Music.find();
+
+  res.render("index", { playlist });
 });
 
 app.get("/admin", (req, res) => {
   res.render("admin");
-  
 });
 
-app.post("/create", async (req, res)=>{
+app.post("/create", async (req, res) => {
   const music = req.body;
-await Music.create(music);
-res.redirect("/")
-
-})
+  await Music.create(music);
+  res.redirect("/");
+});
 
 app.listen(port, () =>
   console.log(`Servidor rodando http://localhost:${port}`)
